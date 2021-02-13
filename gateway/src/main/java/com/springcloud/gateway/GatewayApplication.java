935bd69fdb3d1bef644d2fb17f30dc493e372221
TaoJun0319 <taojun2008nju@163.com>
2021-02-13 14:58:43 +0800

    Commit 添加gateway模块

--- gateway/src/main/java/com/springcloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/springcloud/gateway/GatewayApplication.java
@@ -3,9 +3,11 @@ package com.springcloud.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableHystrix
 @ComponentScan(basePackages = "com.springcloud.gateway")
 @Slf4j
 public class GatewayApplication {
